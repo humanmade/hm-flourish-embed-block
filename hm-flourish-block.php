d54f88b5e54c2f74a09f5f24ed21694ce9ce81a0
Matthew Haines-Young <matthew@humanmade.com>
2026-02-19 20:29:12 +0000

    Initial commit

---
+++ hm-flourish-block.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Flourish Block
+ * Description: Flourish data embed blocks
+ */
+
+namespace HM\Flourish;
+
+const PLUGIN_DIR_PATH = __DIR__;
+
+require_once __DIR__ . '/inc/namespace.php';
+
+bootstrap();
---
+++ inc/namespace.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Namespace functions.
+ */
+
+namespace HM\Flourish;
+
+/**
+ * Add hooks.
+ */
+function bootstrap(): void {
+	// Register hooks and blocks here.
+}
