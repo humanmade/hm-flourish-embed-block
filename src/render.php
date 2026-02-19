4fa7d4b28e81c6785d2c822b4e13d55ab130c45b
Matthew Haines-Young <matthew@humanmade.com>
2026-02-19 21:49:42 +0000

    Simplify render

--- src/render.php
+++ src/render.php
@@ -21,10 +21,4 @@ $wrapper_attributes = get_block_wrapper_attributes();
 
 echo '<div ' . wp_kses_data( $wrapper_attributes ) . '>';
 echo '<div class="flourish-embed flourish-chart" data-src="' . esc_attr( $data_src ) . '"></div>';
-
-// For ServerSideRender (REST context), include script inline since it runs in an iframe.
-// For frontend, view_script handles it.
-if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
-	echo '<script src="https://public.flourish.studio/resources/embed.js"></script>';
-}
 echo '</div>';
